783041eb8910806c892088d56409b2049badc7a6
hs <tlsgustn93@naver.com>
2024-02-17 18:20:13 +0900

    Style/page 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-import { Maintab, Profiletab } from "./_component/Tabs";
-
 export default function Home() {
-  const profileData = {
-    totalCount: 10,
-    votedCount: 5,
-    writtenCount: 7,
-  };
-
-  return (
-    <main>
-      <Maintab />
-      <Profiletab {...profileData} />
-    </main>
-  );
+  return <main></main>;
 }
